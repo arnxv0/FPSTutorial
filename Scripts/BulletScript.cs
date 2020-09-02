5317bc9319af1c0eedc1c77accdc6bbc2763d588
Arnav Dewan <57629464+arnxv0@users.noreply.github.com>
2020-09-02 11:08:23 +0530

    Add completed project and scripts

---
+++ Scripts/BulletScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    public GameObject bullet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == "Target")
+        {
+            Destroy(collision.gameObject);
+            Destroy(bullet);
+        }
+    }
+}
